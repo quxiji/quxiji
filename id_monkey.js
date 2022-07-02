0f1b05ec4e98b5f6b957d476df1e29ad8d128397
quxiji <57881657+quxiji@users.noreply.github.com>
2022-07-02 20:23:09 +0800

    Update id_monkey.js

--- id_monkey.js
+++ id_monkey.js
@@ -177,7 +177,7 @@ function rukou_ondblclick(who){
       case 'id_tool_changxian': break;
       case 'id_tool_pigai':     break;
       case 'id_tool_clear':     break;
-      default:                  tool_in("clear","清空"); tool_in_add_pigai();  tool_in_add_changjietu(); 
+      default:                  tool_in("clear","清空"); tool_in_add_pigai(); tool_in_add_link(); tool_in_add_changjietu(); 
     } 
   }
 }
